7be283df37b042c9e3be53e2934cca7d156910b2
Harold Karibiye <30294430+algo-1@users.noreply.github.com>
2020-08-17 04:10:53 +0100

    Update permutation.py

--- permutation.py
+++ permutation.py
@@ -5,18 +5,6 @@ def get_permutations(sequence):
 
     sequence (string): an arbitrary string to permute. Assume that it is a
     non-empty string.  
-
-    You MUST use recursion for this part. Non-recursive solutions will not be
-    accepted.
-
-    Returns: a list of all permutations of sequence
-
-    Example:
-    >>> get_permutations('abc')
-    ['abc', 'acb', 'bac', 'bca', 'cab', 'cba']
-
-    Note: depending on your implementation, you may return the permutations in
-    a different order than what is listed here.
     '''
     # case for 1 letter 
     if len(sequence) == 1:
@@ -45,18 +33,9 @@ def get_permutations(sequence):
                 else:
                     list_of_insertions.append(first_letter + i[j:])
     return list_of_insertions
-    #pass #delete this line and replace with your code here
 
 if __name__ == '__main__':
-#    #EXAMPLE
-    example_input = 'abc'
-    print('Input:', example_input)
-    print('Expected Output:', ['abc', 'acb', 'bac', 'bca', 'cab', 'cba'])
-    print('Actual Output:', get_permutations(example_input))
-    
-#    # Put three example test cases here (for your sanity, limit your inputs
-#    to be three characters or fewer as you will have n! permutations for a 
-#    sequence of length n)
+    # tests 
     test1 = "abcd"
     print("Input:", test1)
     print("Expected Output:", ["too large"], "n = 24")
